1cc90a76280df6d05cda1b26be8f01aecf0698c9
Flávio Santos <flavioraph@live.com>
2021-07-16 14:51:55 -0300

    Revisao Titulos

--- katas3.js
+++ katas3.js
@@ -1,10 +1,15 @@
 const sampleArray = [469, 755, 244, 245, 758, 450, 302, 20, 712, 71, 456, 21, 398, 339, 882, 848, 179, 535, 940, 472];
 
 
-function showResults(valor) {
+function showResults(valor, nomeExercicio) {
     const body = document.querySelector("body");
     const elemento = document.createElement("p")
+    const elementoTitulo = document.createElement("h1")
     let texto = document.createTextNode("Valor: " + valor);
+    let textoNome = document.createTextNode(nomeExercicio);
+
+    elementoTitulo.appendChild(textoNome)
+    elemento.appendChild(elementoTitulo)
     elemento.appendChild(texto);
     body.appendChild(elemento);
 }
@@ -17,17 +22,16 @@ function kata1() {
     }
     return outPut;
 }
-showResults(kata1());
+showResults(kata1(), "kata1");
 
 function kata2() {
-
     let outPut = []
     for (let i = 25; i >= 1; i--) {
         outPut.push(i)
     }
     return outPut
 }
-showResults(kata2());
+showResults(kata2(), "kata2");
 
 function kata3() {
     let outPut = [];
@@ -36,7 +40,7 @@ function kata3() {
     }
     return outPut;
 }
-showResults(kata3());
+showResults(kata3(), "kata3");
 
 function kata4() {
     let outPut = []
@@ -45,7 +49,7 @@ function kata4() {
     }
     return outPut
 }
-showResults(kata4());
+showResults(kata4(), "kata4");
 
 function kata5() {
     let outPut = []
@@ -56,7 +60,7 @@ function kata5() {
     }
     return outPut
 }
-showResults(kata5());
+showResults(kata5(), "kata5");
 
 
 function kata6() {
@@ -69,7 +73,7 @@ function kata6() {
     }
     return outPut
 }
-showResults(kata6());
+showResults(kata6(), "kata6");
 
 function kata7() {
     let outPut = []
@@ -81,7 +85,7 @@ function kata7() {
     }
     return outPut
 }
-showResults(kata7());
+showResults(kata7(), "kata7");
 
 
 function kata8() {
@@ -94,7 +98,7 @@ function kata8() {
     }
     return outPut
 }
-showResults(kata8());
+showResults(kata8(), "kata8");
 
 function kata9() {
     let outPut = []
@@ -106,7 +110,7 @@ function kata9() {
     }
     return outPut
 }
-showResults(kata9());
+showResults(kata9(), "kata9");
 
 
 function kata10() {
@@ -117,7 +121,7 @@ function kata10() {
     }
     return outPut
 }
-showResults(kata10());
+showResults(kata10(), "kata10");
 
 function kata11() {
     let outPut = []
@@ -129,7 +133,7 @@ function kata11() {
     }
     return outPut
 }
-showResults(kata11());
+showResults(kata11(), "kata11");
 
 
 function kata12() {
@@ -142,7 +146,7 @@ function kata12() {
     }
     return outPut
 }
-showResults(kata12());
+showResults(kata12(), "kata12");
 
 
 function kata13() {
@@ -155,7 +159,7 @@ function kata13() {
     }
     return outPut
 }
-showResults(kata13());
+showResults(kata13(), "kata13");
 
 function kata14() {
     let outPut = []
@@ -165,14 +169,14 @@ function kata14() {
     }
     return outPut
 }
-showResults(kata14());
+showResults(kata14(), "kata14");
 
 function kata15() {
     let i = 1;
     let result = 20 * (20 + 1) / 2
     return result;
 }
-showResults(kata15());
+showResults(kata15(), "kata15");
 
 function kata16() {
     let soma = 0;
@@ -181,7 +185,7 @@ function kata16() {
     }
     return soma;
 }
-showResults(kata16())
+showResults(kata16(), "kata16")
 
 function kata17() {
     let atual = sampleArray[0]
@@ -194,7 +198,7 @@ function kata17() {
     }
     return menor;
 }
-showResults(kata17());
+showResults(kata17(), "kata17");
 
 
 function kata18() {
@@ -209,7 +213,7 @@ function kata18() {
     return maior;
 
 }
-showResults(kata18());
+showResults(kata18(), "kata18");
 
 
 /**
@@ -218,6 +222,10 @@ showResults(kata18());
 
 function kataBonus1() {
     const body = document.querySelector("body");
+    const h1 = document.createElement("h1");
+    let texto = document.createTextNode("BONUS 1")
+    h1.appendChild(texto)
+    body.appendChild(h1)
     for (let i = 0; i < 20; i++) {
         const retangulo = document.createElement("div");
         retangulo.className = "retangulo";
@@ -228,8 +236,12 @@ function kataBonus1() {
 }
 
 function kataBonus2() {
-    let largura = 100;
     const body = document.querySelector("body");
+    const h1 = document.createElement("h2");
+    let texto = document.createTextNode("BONUS 2")
+    h1.appendChild(texto)
+    body.appendChild(h1)
+    let largura = 100;
     for (let i = 0; i < 20; i++) {
         const retangulo = document.createElement("div");
         retangulo.className = "retaguloAcumulativo";
@@ -241,7 +253,12 @@ function kataBonus2() {
 }
 
 function kataBonus3() {
+
     const body = document.querySelector("body");
+    const h1 = document.createElement("h1");
+    let texto = document.createTextNode("BONUS 3")
+    h1.appendChild(texto)
+    body.appendChild(h1)
     for (let i = 0; i < 20; i++) {
         const retangulo = document.createElement("div");
         retangulo.className = "retaguloArray";
@@ -252,6 +269,10 @@ function kataBonus3() {
 
 function kataBonus4() {
     const body = document.querySelector("body");
+    const h1 = document.createElement("h1");
+    let texto = document.createTextNode("BONUS 4")
+    h1.appendChild(texto)
+    body.appendChild(h1)
     for (let i = 0; i < 20; i++) {
         const retangulo = document.createElement("div");
         if (i % 2 == 0) {
@@ -270,6 +291,10 @@ function kataBonus4() {
 
 function kataBonus5() {
     const body = document.querySelector("body");
+    const h1 = document.createElement("h1");
+    let texto = document.createTextNode("BONUS 5")
+    h1.appendChild(texto)
+    body.appendChild(h1)
     for (let i = 0; i < 20; i++) {
         const retangulo = document.createElement("div");
         if (sampleArray[i] % 2 == 0) {
